4732c4546efb8a378ef5e3029709688f0d8d28b6
Recupero <giorecupero@gmail.com>
2020-10-29 14:48:34 +0100

    Update conference controller with path parameter

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,19 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/conference", name="conference")
-     * @Route("/", name="homepage")
+     * @Route("/{name}", name="homepage")
      */
-    public function index(): Response
+    public function index(string $name = ''): Response
     {
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', \htmlspecialchars($name));
+        }
+        // dump($request);
         return new Response(<<<EOF
                 <html>
                     <body>
+                        $greet
                         <img src="/images/megazord.png"/>
                     </body>       
                 </html>
